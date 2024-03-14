ee4594e7284a861b25ec637f7b7afd3ab79efbe0
git-zaidism <zaidisim7@gmail.com>
2024-03-14 23:36:16 +0530

    "Custom Exception implementation"

---
+++ src/main/java/org/zaidism/exceptions/ErrorResponse.java
@@ -0,0 +1,14 @@
+package org.zaidism.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@AllArgsConstructor
+@Data
+public class ErrorResponse {
+    Set<String> errorMessage;
+}
